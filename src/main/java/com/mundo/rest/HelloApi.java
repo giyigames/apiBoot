0506885b4cdf1797ce1a680191e1c7c1a434e524
giyigames <ciber.xploits@gmail.com>
2023-05-18 22:21:06 -0500

    original

--- src/main/java/com/mundo/rest/HelloApi.java
+++ src/main/java/com/mundo/rest/HelloApi.java
@@ -1,14 +1,13 @@
 package com.mundo.rest;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class HelloApi {
+public class  HelloApi{
     public static void main(String[] args) {
         SpringApplication.run(HelloApi.class, args);
     }
@@ -17,8 +16,25 @@ public class HelloApi {
 @RestController
 class HelloWorldController {
     @GetMapping("/hello")
-    public String helloWorld() {
-        return "Hello World";
+    public ResponseEntity<?> helloWorld() {
+        String message = "Hello World";
+        return ResponseEntity.ok(new Greeting(message));
+    }
+}
+
+class Greeting {
+    private String saludo;
+
+    public Greeting(String saludo) {
+        this.saludo = saludo;
+    }
+
+    public String getSaludo() {
+        return saludo;
+    }
+
+    public void setSaludo(String saludo) {
+        this.saludo = saludo;
     }
 }
 
